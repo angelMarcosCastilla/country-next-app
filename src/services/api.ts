17d0bea78f7adbd0f81615486f975eb49e366a70
angelMarcosCastilla <ajmarcosc15@gmail.com>
2023-04-03 11:09:45 -0500

    delay in get countries

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,6 +8,9 @@ interface ApiI {
 
 export const api: ApiI = {
   allCountry: async () => {
+    // detemos 10 segundos para ver el loading
+    // vamos a probar que se genere los estaticos
+    await new Promise((resolve) => setTimeout(resolve, 10000)) 
     const res = await fetch('https://restcountries.com/v3.1/all')
     const data = await res.json()
     return data
